5a90a79d6faef7f22b5ee1aecc4cb44f0847ec43
CORP\taoy <yida.tao@opco.com>
2024-05-21 10:43:15 -0400

    remove CardinalResponse

--- src/main/java/com/opco/openshift/dcm/controller/SystemInfoController.java
+++ src/main/java/com/opco/openshift/dcm/controller/SystemInfoController.java
@@ -1,7 +1,6 @@
 package com.opco.syndicate.dcm.controller;
 
 
-import com.opco.cardinal.rest.CardinalResponse;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +14,8 @@ public class SystemInfoController {
 
 
     @RequestMapping(path = "/systemInfo", method = RequestMethod.GET)
-    public CardinalResponse status() {
-        return CardinalResponse.success("success");
+    public String status() {
+        return "Success";
     }
     
 }
