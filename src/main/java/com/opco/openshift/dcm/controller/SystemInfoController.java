526efb94273c2dc9263ca9b3f7c945899fdbea4f
tyd2024 <168551273+tyd2024@users.noreply.github.com>
2024-09-30 11:09:15 -0400

    Update SystemInfoController.java

--- src/main/java/com/opco/openshift/dcm/controller/SystemInfoController.java
+++ src/main/java/com/opco/openshift/dcm/controller/SystemInfoController.java
@@ -4,6 +4,7 @@ package com.opco.syndicate.dcm.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 /**
  *
